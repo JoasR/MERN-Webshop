39e651c49f538c9e56456f1d6ccb54d2c1684c12
JoasR <joas.rothig@hotmail.com>
2023-01-17 14:04:31 +0100

    Duplicated Repo
    Duplicated Repo. This way I could seperate the webshop from other projects. Thats why folders have been deleted in previous commits.

--- react-webshop/src/App.jsx
+++ react-webshop/src/App.jsx
@@ -1,5 +1,5 @@
-import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";

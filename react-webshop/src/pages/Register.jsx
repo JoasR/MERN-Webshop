8bf57ae6b5bca25dc14346ed31359083bdac595c
JoasR <joas.rothig@hotmail.com>
2023-02-16 16:31:47 +0100

    Added Validation to registration
    Added validations to the registration Page

--- react-webshop/src/pages/Register.jsx
+++ react-webshop/src/pages/Register.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { mobile } from '../responsive'
+import { useState } from "react"
 
 const Container = styled.div`
     width: 100vw;
@@ -35,7 +36,6 @@ const Form = styled.form`
 `
 
 const Input = styled.input`
-    flex: 1;
     min-width: 40%;
     margin: 20px 10px 0 0;
     padding: 10px;
@@ -53,25 +53,226 @@ const Button = styled.button`
     background-color: teal;
     color: white;
     cursor: pointer;
+
+    &:disabled{
+        opacity: 0.4;
+        cursor: not-allowed;
+    }
+`
+
+const ErrorMessage = styled.span`
+    font-size: 12px;
+    color: red;
+`
+
+const InputWrapper = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    min-width: 40%;
 `
 
 const Register = () => {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [isFormValid, setIsFormValid] = useState(false);
+    const [focusFirstName, setFocusFirstName] = useState(false)
+    const [focusLastName, setFocusLastName] = useState(false)
+    const [focusUsername, setFocusUsername] = useState(false)
+    const [focusEmail, setFocusEmail] = useState(false)
+    const [focusPassword, setFocusPassword] = useState(false)
+    const [focusConfirmPassword, setFocusConfirmPassword] = useState(false)
+
+      useEffect(() => {
+        function validateForm() {
+            // Check if all the required fields are filled out
+            if (
+              firstName.trim() === '' ||
+              lastName.trim() === '' ||
+              username.trim() === '' ||
+              email.trim() === '' ||
+              password.trim() === '' ||
+              confirmPassword.trim() === ''
+            ) {
+              // If any of the required fields are empty, the form is not valid
+              setIsFormValid(false);
+            } else if (username.trim().length < 3 || username.trim().length > 16) {
+              // If the username is too short or too long, the form is not valid
+              setIsFormValid(false);
+            } else if (!isValidEmail(email)) {
+              // If the email is not valid, the form is not valid
+              setIsFormValid(false);
+            } else if (password !== confirmPassword) {
+              // If the passwords do not match, the form is not valid
+              setIsFormValid(false);
+            } else if (!isValidPassword(password)) {
+              // If the password is not valid, the form is not valid
+              setIsFormValid(false);
+            } else {
+              // If all the required fields are filled out and the additional validations pass, the form is valid
+              setIsFormValid(true);
+            }
+          }
+          validateForm()
+      }, [password, email, username, firstName, lastName, confirmPassword, isFormValid])
+      
+      function isValidPassword(password) {
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\S]{6,}$/;
+        return passwordRegex.test(password);
+      }
+      
+      function handleChangeFirstName(e) {
+        setFirstName(e.target.value);
+      }
+      
+      function handleChangeLastName(e) {
+        setLastName(e.target.value);
+      }
+      
+      function handleChangeUsername(e) {
+        setUsername(e.target.value);
+      }
+      
+      function handleChangeEmail(e) {
+        setEmail(e.target.value);
+      }
+      
+      function handleChangePassword(e) {
+        setPassword(e.target.value);
+      }
+      
+      function handleChangeConfirmPassword(e) {
+        setConfirmPassword(e.target.value);
+      }
+      
+      function isValidEmail(email) {
+        // A basic email validation regex
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      }
+
+    const handleFirstNameFocus = (e) => {
+        setFocusFirstName(true)
+    }
+    const handleLastNameFocus = (e) => {
+        setFocusLastName(true)
+    }
+    const handleUsernameFocus = (e) => {
+        setFocusUsername(true)
+    }
+    const handleEmailFocus = (e) => {
+        setFocusEmail(true)
+    }
+    const handlePasswordFocus = (e) => {
+        setFocusPassword(true)
+    }
+    const handleConfirmPasswordFocus = (e) => {
+        setFocusConfirmPassword(true)
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        console.log("account created successfully: " + firstName + lastName + email + username + password + confirmPassword)
+    }
+
+    //this validations only happen when the button is clicked. can you make it so these validations are done live, so ill instantly see the alerts as soon as the user uses the inputs?
   return (
     <Container>
         <Wrapper>
             <Title>CREATE AN ACCOUNT</Title>
             <Form>
-                <Input placeholder="name"/>
-                <Input placeholder="last name"/>
-                <Input placeholder="username"/>
-                <Input placeholder="email"/>
-                <Input placeholder="password"/>
-                <Input placeholder="confirm password"/>
+                <InputWrapper>
+                <Input
+                    type="text"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={handleChangeFirstName}
+                    name="first_name"
+                    required
+                    onBlur={handleFirstNameFocus}
+                />
+                {(firstName.trim() === '' && focusFirstName) && <ErrorMessage>Please enter your first name.</ErrorMessage>}
+                </InputWrapper>
+                <InputWrapper>
+                <Input
+                    type="text"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={handleChangeLastName}
+                    name="last_name"
+                    required
+                    onBlur={handleLastNameFocus}
+                />
+                {(lastName.trim() === '' && focusLastName) && <ErrorMessage>Please enter your last name.</ErrorMessage>}
+                </InputWrapper>
+                <InputWrapper>
+                <Input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={handleChangeUsername}
+                    name="username"
+                    required
+                    onBlur={handleUsernameFocus}
+                />
+                {((username.trim() === '' || username.trim().length < 3 || username.trim().length > 16) && focusUsername) && (
+                    <ErrorMessage>
+                    Username must be between 3 and 16 characters.
+                    </ErrorMessage>
+                )}
+                </InputWrapper>
+                <InputWrapper>
+                <Input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={handleChangeEmail}
+                    name="email"
+                    required
+                    onBlur={handleEmailFocus}
+                />
+                {(!isValidEmail(email) && focusEmail) && <ErrorMessage>Please enter a valid email address.</ErrorMessage>}
+                </InputWrapper>
+                <InputWrapper>
+                <Input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={handleChangePassword}
+                    name="password"
+                    required
+                    onBlur={handlePasswordFocus}
+                />
+                {(!isValidPassword(password) && focusPassword) && (
+                    <ErrorMessage>Password should be atleast be 6 characters long and contain atleast 1 number and 1 capitalised letter.</ErrorMessage>
+                )}
+                </InputWrapper>
+                <InputWrapper>
+                <Input
+                    type="password"
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={handleChangeConfirmPassword}
+                    name="confirm_password"
+                    required
+                    onBlur={handleConfirmPasswordFocus}
+                />
+                {(password !== confirmPassword && focusConfirmPassword) && (
+                    <ErrorMessage>The passwords do not match.</ErrorMessage>
+                )}
+                </InputWrapper>
                 <Agreement>
-                    By creating an account, I consent to the processing of my 
-                    personal data in accordance with the <b>PRIVACY POLICY</b>
+                By creating an account, I consent to the processing of my personal data in accordance with the{' '}
+                <b>PRIVACY POLICY</b>
                 </Agreement>
-                <Button>CREATE</Button>
+                <Button disabled={!isFormValid} onClick={handleClick}>
+                CREATE
+                </Button>
             </Form>
         </Wrapper>
     </Container>
